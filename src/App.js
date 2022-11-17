45d4b81c44d8eaa625efedcb10834eebee90c938
blase147 <solarmails2@gmail.com>
2022-11-17 13:48:26 +0100

    Update files, folders

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Quote from './pages/Quote';
-import Calculator from './components/calculator';
-import Navbar from './pages/Navbar';
+import Calculator from './components/calculatorPage/calculator';
+import Quotes from './components/quotePage/quote';
+import HomePage from './components/homePage/home';
+import Navbar from './components/navbarHeading/navbar';
 
-function App() {
-  return (
-    <>
-      <Navbar />
-      <Routes>
-        <Route path="/quote" element={<Quote />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/calculator" element={<Calculator />} />
-      </Routes>
-    </>
-  );
+class App extends React.PureComponent {
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/calculator" element={<Calculator />} />
+          <Route path="/quote" element={<Quotes />} />
+        </Routes>
+      </div>
+    );
+  }
 }
 
 export default App;
